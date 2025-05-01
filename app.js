cffadfe709749619d94a842618c165b039d50233
theuzomavictor <siruzomav96@gmail.com>
2025-05-01 18:07:37 +0100

    Finish functionality and styling of game
    Users can now play and enjoy the game
    with a beautiful interface and a smooth
    experience.

--- app.js
+++ app.js
@@ -4,79 +4,84 @@ const scissorsEl = document.getElementById('scissors');
 const scoreEl = document.getElementById('score-el');
 const playAgainEl = document.getElementById('play-again');
 const userPickEl = document.getElementById('user-pick');
+const housePickEl = document.getElementById('house-pick');
+const resultText = document.getElementById('result-text');
 const rulesBtnEl = document.getElementById('rules-btn');
 const closeBtnEl = document.getElementById('close-btn');
 const modalEl = document.getElementById('modal-container');
+const battleScreen = document.querySelector('.battle-screen');
+const gameScreen = document.querySelector('.game-screen');
+const newGameEl = document.getElementById('new-game');
 
-const gameOptions = ['Rock', 'Paper', 'Scissors'];
 let score = 0;
 scoreEl.textContent = score;
 
+function handleButtonClick(userChoice) {
+    userPickEl.innerHTML = `<img src="./images/icon-${userChoice}.svg" alt="${userChoice}" class="rock">`
 
-rockEl.addEventListener('click', function() {
-    userPickEl.innerHTML = `
-    <img src="./images/icon-rock.svg" alt="" class="rock">
-    `
+    gameScreen.classList.add('hidden');
 
-    const housePick = gameOptions[Math.floor(Math.random() * gameOptions.length)];
-    console.log(housePick);
-    if (housePick === 'Scissors') {
-        console.log('You Win.')
-        score += 1;
-        scoreEl.textContent = score
-    } else if (housePick === 'Rock') {
-        console.log('Draw.')
-        score = score
-        scoreEl.textContent = score
-    } else {
-        console.log('You Lose.')
-        score = score - 1;
-        scoreEl.textContent = score
+    battleScreen.classList.remove('hidden');
+
+    setTimeout(() => {
+        const computerChoice = getComputerChoice();
+        housePickEl.textContent = '?'
+        housePickEl.innerHTML = `<img src="./images/icon-${computerChoice}.svg" alt="${computerChoice}" class="rock">`
+
+        updateScore(userChoice, computerChoice)
+    }, 1000);
+
+    housePickEl.textContent = '?'
+    resultText.textContent = ''
+};
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const housePick = Math.floor(Math.random() * choices.length);
+    return  choices[housePick];
+};
+
+function updateScore(user, computer) {
+    if (user === computer) {
+        resultText.textContent = 'YOU DRAW 🤝'
+        return;
     }
-})
+    
+    const userWins = 
+    (user === 'rock' && computer === 'scissors') || 
+    (user === 'paper' && computer === 'rock') || 
+    (user === 'scissors' && computer === 'paper');
 
-paperEl.addEventListener('click', function() {
-    const housePick = gameOptions[Math.floor(Math.random() * gameOptions.length)];
-    console.log(housePick);
-    if (housePick === 'Scissors') {
-        console.log('You Lose.')
-        score -= 1;
-        scoreEl.textContent = score
-    } else if (housePick === 'Rock') {
-        console.log('You Win.')
-        score += 1;
-        scoreEl.textContent = score
+    if (userWins) {
+        score++;
+        resultText.textContent = 'YOU WIN 🎉';
     } else {
-        console.log('Draw.')
-        score = score;
-        scoreEl.textContent = score
+        score--;
+        resultText.textContent = 'YOU LOSE 🙆';
     }
-})
 
-scissorsEl.addEventListener('click', function() {
-    const housePick = gameOptions[Math.floor(Math.random() * gameOptions.length)];
-    console.log(housePick);
-    if (housePick === 'Scissors') {
-        console.log('Draw.')
-        score = score;
-        scoreEl.textContent = score
-    } else if (housePick === 'Rock') {
-        console.log('You Lose.')
-        score -= 1;
-        scoreEl.textContent = score
-    } else {
-        console.log('You Win.')
-        score += 1;
-        scoreEl.textContent = score
-    }
-})
+    scoreEl.textContent = score;
+};
+
+rockEl.addEventListener('click', () => handleButtonClick('rock'));
+paperEl.addEventListener('click', () => handleButtonClick('paper'));
+scissorsEl.addEventListener('click', () => handleButtonClick('scissors'));
 
 rulesBtnEl.addEventListener('click', function() {
     modalEl.style.display = 'block';
-})
+});
 
 closeBtnEl.addEventListener('click', function() {
     modalEl.style.display = 'none';
-})
+});
 
+playAgainEl.addEventListener('click', () => {
+    gameScreen.classList.remove('hidden');
+    battleScreen.classList.add('hidden');
+});
 
+newGameEl.addEventListener('click', () => {
+    gameScreen.classList.remove('hidden');
+    battleScreen.classList.add('hidden');
+    scoreEl.textContent = 0 
+});
